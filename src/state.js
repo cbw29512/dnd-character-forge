ce7f2ee451e06bf2851e7d0316eb20910d7f0d8d
Chris Wilson <divclass01@gmail.com>
2026-08-21 21:15:41 -0400

    Include all class advancement levels in state schema

--- src/state.js
+++ src/state.js
@@ -7,7 +7,7 @@ export function createInitialState() {
       ruleset: "2024",
       constraints: { level:RANDOM, species:RANDOM, class:RANDOM, subclass:RANDOM, background:RANDOM, name:"" },
       spellSelections: { cantrips:[], spellbook:[], prepared:[] },
-      advancementSelections: { 4:RANDOM, 8:RANDOM, 12:RANDOM, 16:RANDOM, 19:RANDOM },
+      advancementSelections: { 4:RANDOM, 6:RANDOM, 8:RANDOM, 10:RANDOM, 12:RANDOM, 14:RANDOM, 16:RANDOM, 19:RANDOM },
       homebrew: [],
       currentCharacter: null,
       activeTab: "forge"
