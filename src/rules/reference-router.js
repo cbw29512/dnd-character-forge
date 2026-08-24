b9b312636a81d1f62e2b9eea9e7bfc4a9679e5e9
Chris Wilson <divclass01@gmail.com>
2026-08-24 07:52:51 -0400

    feat: route Monk to sourced play reference

--- src/rules/reference-router.js
+++ src/rules/reference-router.js
@@ -1,6 +1,7 @@
 import { buildQuickReference as buildCoreQuickReference } from "./reference.js";
 import { buildBarbarianQuickReference } from "./barbarian-reference.js";
 import { buildBardQuickReference } from "./bard-reference.js";
+import { buildMonkQuickReference } from "./monk-reference.js";
 import { buildDruidQuickReference } from "./druid-reference.js";
 import { buildPaladinQuickReference } from "./paladin-reference.js";
 import { buildRangerQuickReference } from "./ranger-reference.js";
@@ -9,6 +10,7 @@ export function buildQuickReference(character){
   try{
     if(character?.class?.id==="barbarian")return buildBarbarianQuickReference(character);
     if(character?.class?.id==="bard")return buildBardQuickReference(character);
+    if(character?.class?.id==="monk")return buildMonkQuickReference(character);
     if(character?.class?.id==="druid")return buildDruidQuickReference(character);
     if(character?.class?.id==="paladin")return buildPaladinQuickReference(character);
     if(character?.class?.id==="ranger")return buildRangerQuickReference(character);
