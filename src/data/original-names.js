4bf5c279491a92b7b115ee24195703013904dd95
Chris Wilson <divclass01@gmail.com>
2026-09-03 09:36:27 -0400

    Add broad Forge Original character name pools

---
+++ src/data/original-names.js
@@ -0,0 +1,20 @@
+export const FORGE_GIVEN_NAMES=Object.freeze([
+  "Alden","Alira","Aric","Arlen","Aster","Bren","Briala","Cael","Cassian","Cira","Corin","Dain","Dara",
+  "Elian","Elira","Elowen","Eryn","Fenn","Galen","Garrick","Halia","Ilya","Jalen","Jora","Kael","Kessa",
+  "Kieran","Kora","Liora","Lucan","Maelis","Mara","Marek","Mira","Nalia","Nerys","Nyra","Oren","Orla",
+  "Perrin","Rhea","Rian","Riven","Rowan","Sable","Selene","Sera","Seren","Sorrel","Talia","Taren","Tavian",
+  "Tessa","Theron","Toren","Vara","Veya","Wren","Wynn","Ysra","Yorin","Zara","Zev","Adra","Belden","Celyn",
+  "Daelin","Eira","Esra","Fara","Fenric","Hest","Isen","Jessa","Kelan","Linnea","Merek","Odran"
+]);
+
+export const FORGE_FAMILY_NAMES=Object.freeze([
+  "Ashford","Alderbrook","Amberwick","Blackmere","Blackthorn","Briar","Brighthollow","Brightwood","Calder","Cinder",
+  "Cloudmere","Crestfall","Crowe","Dawnmere","Deepwell","Emberfall","Fairwind","Farrow","Flint","Frostmere","Gale",
+  "Goldleaf","Graymark","Greenbriar","Hart","Hawke","Holloway","Ironwood","Keel","Larkspur","Marsh","Moonfall","Moss",
+  "Nightbrook","Oakheart","Pike","Quill","Rainmere","Ravenwood","Reed","Ridge","Riverstone","Sage","Silverbrook","Slate",
+  "Stone","Stormmere","Thorne","Vale","Voss","Westmere","Wilding","Wintermere","Woodfall","Bramble","Duskfall","Evermere",
+  "Foxglove","Highwater","Lowen","Mistvale","Northwind","Rook","Starling","Stillwater","Sunmere","Thistle","Wayfarer",
+  "Willowmere","Wolfe","Ashvale","Boulder","Clearwater","Duskwood","Emberwick","Frostvale","Glen","Harbor","Irongate","Juniper"
+]);
+
+export const FORGE_NAME_COMBINATIONS=FORGE_GIVEN_NAMES.length*FORGE_FAMILY_NAMES.length;
